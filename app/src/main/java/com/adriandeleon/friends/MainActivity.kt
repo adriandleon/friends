170a142460f2db5b95426a9eb608b19b18d3b252
Adrian De León <adriandleon@gmail.com>
2021-06-21 17:12:00 -0300

    Removed the app compat library dependency

--- app/src/main/java/com/adriandeleon/friends/MainActivity.kt
+++ app/src/main/java/com/adriandeleon/friends/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.adriandeleon.friends.ui.theme.FriendsTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
