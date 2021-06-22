3a57df65a38fa89ae911a52ee205ba345316aac8
Adrian De León <adriandleon@gmail.com>
2021-06-22 14:32:04 -0300

    SignUp screen fields interactions

--- app/src/main/java/com/adriandeleon/friends/signup/SignUp.kt
+++ app/src/main/java/com/adriandeleon/friends/signup/SignUp.kt
@@ -1,14 +1,16 @@
 package com.adriandeleon.friends.signup
 
 import android.content.res.Configuration.UI_MODE_TYPE_NORMAL
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,35 +24,21 @@ fun SignUp() {
             .fillMaxSize()
             .padding(16.dp)
     ) {
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = stringResource(id = R.string.createAnAccount),
-                style = typography.h4
-            )
-        }
+        ScreenTitle(R.string.createAnAccount)
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            label = {
-                Text(text = stringResource(id = R.string.email))
-            },
-            onValueChange = {}
+        EmailField(
+            value = email,
+            onValueChange = { email = it }
         )
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            label = {
-                Text(text = stringResource(id = R.string.password))
-            },
-            onValueChange = {}
+        PasswordField(
+            value = password,
+            onValueChange = { password = it }
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -62,6 +50,64 @@ fun SignUp() {
             Text(text = stringResource(id = R.string.signUp))
         }
     }
+}
+
+@Composable
+private fun ScreenTitle(@StringRes resourceId: Int) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(id = resourceId),
+            style = typography.h4
+        )
+    }
+}
+
+@Composable
+private fun EmailField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        label = {
+            Text(text = stringResource(id = R.string.email))
+        },
+        onValueChange = onValueChange
+    )
+}
+
+@Composable
+private fun PasswordField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    var isVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        trailingIcon = {
+            IconButton(onClick = { isVisible = !isVisible }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_visibility),
+                    contentDescription = stringResource(id = R.string.toggleVisibility)
+                )
+            }
+        },
+        visualTransformation = if (isVisible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        label = {
+            Text(text = stringResource(id = R.string.password))
+        },
+        onValueChange = onValueChange
+    )
 }
 
 @Composable
