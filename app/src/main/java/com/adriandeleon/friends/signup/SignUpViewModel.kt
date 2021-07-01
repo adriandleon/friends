b403c9cd4f2806302d039be407c8bf49f24741c3
Adrian De León <adriandleon@gmail.com>
2021-07-01 15:02:23 -0300

    Removed unused code

--- app/src/main/java/com/adriandeleon/friends/signup/SignUpViewModel.kt
+++ app/src/main/java/com/adriandeleon/friends/signup/SignUpViewModel.kt
@@ -70,33 +70,5 @@ class SignUpViewModel(private val credentialsValidator: RegexCredentialsValidato
         }
     }
 
-    private fun createUser(
-        email: String,
-        password: String,
-        about: String
-    ): User {
-        checkAccountExists(email)
-        val userId = createUserIdFor(email) + "Id"
-        val user = User(userId, email, about)
-        saveUser(password, user)
-        return user
-    }
-
-    private fun saveUser(password: String, user: User) {
-        usersForPassword.getOrPut(password, ::mutableListOf).add(user)
-    }
-
-    private fun createUserIdFor(email: String): String {
-        return email.takeWhile { it != '@' }
-    }
-
-    private fun checkAccountExists(email: String) {
-        if (usersForPassword.values.flatten().any { it.email == email }) {
-            throw DuplicateAccountException()
-        }
-    }
-
-    private val usersForPassword = mutableMapOf<String, MutableList<User>>()
-
     class DuplicateAccountException : Throwable()
 }
