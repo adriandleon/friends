7457c6f4a543a401e4e4d5fd7953f6e6175a27e7
Adrian De León <adriandleon@gmail.com>
2021-07-09 00:54:40 -0300

    Introduced the custom application class

---
+++ app/src/main/java/com/adriandeleon/friends/app/FriendsApplication.kt
@@ -0,0 +1,10 @@
+package com.adriandeleon.friends.app
+
+import android.app.Application
+
+class FriendsApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
