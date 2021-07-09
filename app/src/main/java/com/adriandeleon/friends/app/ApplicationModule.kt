da839066fe845b12706ed8ff465eabd9e502b126
Adrian De León <adriandleon@gmail.com>
2021-07-09 01:07:44 -0300

    Defined the application koin module

---
+++ app/src/main/java/com/adriandeleon/friends/app/ApplicationModule.kt
@@ -0,0 +1,21 @@
+package com.adriandeleon.friends.app
+
+import com.adriandeleon.friends.domain.user.InMemoryUserCatalog
+import com.adriandeleon.friends.domain.user.UserRepository
+import com.adriandeleon.friends.domain.validation.RegexCredentialsValidator
+import com.adriandeleon.friends.signup.SignUpViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val applicationModule = module {
+    single { InMemoryUserCatalog() }
+    factory { RegexCredentialsValidator() }
+    factory { UserRepository(userCatalog = get()) }
+
+    viewModel {
+        SignUpViewModel(
+            credentialsValidator = get(),
+            userRepository = get()
+        )
+    }
+}
--- app/src/main/java/com/adriandeleon/friends/app/FriendsApplication.kt
+++ app/src/main/java/com/adriandeleon/friends/app/FriendsApplication.kt
@@ -1,10 +1,15 @@
 package com.adriandeleon.friends.app
 
 import android.app.Application
+import org.koin.core.context.startKoin
 
+@Suppress("unused")
 class FriendsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            modules(applicationModule)
+        }
     }
 }
