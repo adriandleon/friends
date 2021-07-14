e73ed190d5b26cab9f96420768de59b900b61195
Adrian De León <adriandleon@gmail.com>
2021-07-13 23:46:40 -0300

    Can detect when the device is offline

--- app/src/test/java/com/adriandeleon/friends/signup/FailedAccountCreationTest.kt
+++ app/src/test/java/com/adriandeleon/friends/signup/FailedAccountCreationTest.kt
@@ -1,6 +1,7 @@
 package com.adriandeleon.friends.signup
 
 import com.adriandeleon.friends.domain.exceptions.BackendException
+import com.adriandeleon.friends.domain.exceptions.ConnectionUnavailableException
 import com.adriandeleon.friends.domain.user.User
 import com.adriandeleon.friends.domain.user.UserCatalog
 import com.adriandeleon.friends.domain.user.UserRepository
@@ -17,6 +18,20 @@ class FailedAccountCreationTest {
         val result = userRepository.signUp("email", "password", "about")
 
         assertEquals(SignUpState.BackendError, result)
+    }
+
+    @Test
+    fun `offline error`() {
+        val userRepository = UserRepository(OfflineUserCatalog())
+        val result = userRepository.signUp("email", "password", "about")
+        assertEquals(SignUpState.Offline, result)
+    }
+
+    class OfflineUserCatalog : UserCatalog {
+
+        override fun createUser(email: String, password: String, about: String): User {
+            throw ConnectionUnavailableException()
+        }
     }
 
     class UnavailableUserCatalog : UserCatalog {
