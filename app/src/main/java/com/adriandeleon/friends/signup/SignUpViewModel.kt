02e313af298f2ab7dbe106b01b78a76f3ee7b94a
Adrian De León <adriandleon@gmail.com>
2022-04-28 21:14:04 -0300

    Extracted the validation into a separate class

--- app/src/main/java/com/adriandeleon/friends/signup/SignUpViewModel.kt
+++ app/src/main/java/com/adriandeleon/friends/signup/SignUpViewModel.kt
@@ -11,7 +11,7 @@ class SignUpViewModel {
     val signUpState: LiveData<SignUpState> = _mutableSignUpState
 
     fun createAccount(email: String, password: String, about: String) {
-        when (validate(email, password)) {
+        when (RegexCredentialsValidator().validate(email, password)) {
             is CredentialsValidationResult.InvalidEmail ->
                 _mutableSignUpState.value = SignUpState.BadEmail
             is CredentialsValidationResult.InvalidPassword ->
@@ -19,22 +19,25 @@ class SignUpViewModel {
         }
     }
 
-    private fun validate(
-        email: String,
-        password: String
-    ): CredentialsValidationResult {
-        val emailRegex =
-            """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
-        val emailPattern = Pattern.compile(emailRegex)
-        val passwordRegex = """^(?=.*[0-9])(?=.*[a-z])(?=.*A-Z)(?=.*[!@#$%^&*+=\-]).{8,}$"""
-        val passwordPattern = Pattern.compile(passwordRegex)
+    class RegexCredentialsValidator {
 
-        val result = if (!emailPattern.matcher(email).matches()) {
-            CredentialsValidationResult.InvalidEmail
-        } else if (!passwordPattern.matcher(password).matches()) {
-            CredentialsValidationResult.InvalidPassword
-        } else TODO()
-        return result
+        fun validate(
+            email: String,
+            password: String
+        ): CredentialsValidationResult {
+            val emailRegex =
+                """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
+            val emailPattern = Pattern.compile(emailRegex)
+            val passwordRegex = """^(?=.*[0-9])(?=.*[a-z])(?=.*A-Z)(?=.*[!@#$%^&*+=\-]).{8,}$"""
+            val passwordPattern = Pattern.compile(passwordRegex)
+
+            val result = if (!emailPattern.matcher(email).matches()) {
+                CredentialsValidationResult.InvalidEmail
+            } else if (!passwordPattern.matcher(password).matches()) {
+                CredentialsValidationResult.InvalidPassword
+            } else TODO()
+            return result
+        }
     }
 
     sealed class CredentialsValidationResult {
