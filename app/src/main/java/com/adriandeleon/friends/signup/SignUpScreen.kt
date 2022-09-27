cbe4568bb462a483dcbe1a76ae9337120b9421fa
Adrian De León <adriandleon@gmail.com>
2022-09-26 23:11:36 -0300

    Merged the if conditions into a single if-else condition

--- app/src/main/java/com/adriandeleon/friends/signup/SignUpScreen.kt
+++ app/src/main/java/com/adriandeleon/friends/signup/SignUpScreen.kt
@@ -32,6 +32,16 @@ fun SignUpScreen(
 
     if (signUpState is SignUpState.SignedUp) {
         onSignedUp()
+    } else if (signUpState is SignUpState.BadEmail) {
+        isBadEmail = true
+    } else if (signUpState is SignUpState.BadPassword) {
+        isBadPassword = true
+    } else if (signUpState is SignUpState.DuplicateAccount) {
+        InfoMessage(R.string.duplicateAccountError)
+    } else if (signUpState is SignUpState.BackendError) {
+        InfoMessage(stringResource = R.string.createAccountError)
+    } else if (signUpState is SignUpState.Offline) {
+        InfoMessage(stringResource = R.string.offlineError)
     }
 
     Box(modifier = Modifier.fillMaxSize()) {
@@ -65,17 +75,6 @@ fun SignUpScreen(
             ) {
                 Text(text = stringResource(id = R.string.signUp))
             }
-        }
-        if (signUpState is SignUpState.BadEmail) {
-            isBadEmail = true
-        } else if (signUpState is SignUpState.BadPassword) {
-            isBadPassword = true
-        } else if (signUpState is SignUpState.DuplicateAccount) {
-            InfoMessage(R.string.duplicateAccountError)
-        } else if (signUpState is SignUpState.BackendError) {
-            InfoMessage(stringResource = R.string.createAccountError)
-        } else if (signUpState is SignUpState.Offline) {
-            InfoMessage(stringResource = R.string.offlineError)
         }
     }
 }
