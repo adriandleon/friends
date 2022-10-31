8173c1049232da9898ebc295b2301b67d7d508d5
Adrian De León <adriandleon@gmail.com>
2022-10-31 17:08:04 -0300

    Can fetch posts for given user id as long as the user id is timId

--- app/src/test/java/com/adriandeleon/friends/timeline/LoadPostsTest.kt
+++ app/src/test/java/com/adriandeleon/friends/timeline/LoadPostsTest.kt
@@ -1,6 +1,8 @@
 package com.adriandeleon.friends.timeline
 
 import com.adriandeleon.friends.InstantTaskExecutor
+import com.adriandeleon.friends.domain.post.Post
+import com.adriandeleon.friends.domain.user.User
 import com.adriandeleon.friends.timeline.state.TimelineState
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -16,5 +18,16 @@ class LoadPostsTest {
         viewModel.timelineFor("annaId")
 
         assertEquals(TimelineState.Posts(emptyList()), viewModel.timelineState.value)
+    }
+
+    @Test
+    fun `posts available`() {
+        val tim = User("timId", "tim@friends.com", "About Tim")
+        val timPosts = listOf(Post("postId", tim.id, "post text", 1L))
+        val viewModel = TimelineViewModel()
+
+        viewModel.timelineFor(tim.id)
+
+        assertEquals(TimelineState.Posts(timPosts), viewModel.timelineState.value)
     }
 }
