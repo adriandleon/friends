865d8e68a27650735c25f3c53d1f77c892923d5d
Adrian De León <adriandleon@gmail.com>
2023-01-02 22:41:47 -0300

    Extracted the followings into the InMemoryUserCatalog

--- app/src/main/java/com/adriandeleon/friends/timeline/TimelineViewModel.kt
+++ app/src/main/java/com/adriandeleon/friends/timeline/TimelineViewModel.kt
@@ -3,7 +3,7 @@ package com.adriandeleon.friends.timeline
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.adriandeleon.friends.domain.post.InMemoryPostCatalog
-import com.adriandeleon.friends.domain.user.Following
+import com.adriandeleon.friends.domain.user.InMemoryUserCatalog
 import com.adriandeleon.friends.timeline.state.TimelineState
 
 class TimelineViewModel {
@@ -12,18 +12,8 @@ class TimelineViewModel {
     val timelineState: LiveData<TimelineState> = mutableTimelineState
 
     fun timelineFor(userId: String) {
-        val userIds = listOf(userId) + followedBy(userId)
+        val userIds = listOf(userId) + InMemoryUserCatalog().followedBy(userId)
         val postsForUser = InMemoryPostCatalog().postsFor(userIds)
         mutableTimelineState.value = TimelineState.Posts(postsForUser)
-    }
-
-    private fun followedBy(userId: String): List<String> {
-        val followings = listOf(
-            Following("saraId", "lucyId"),
-            Following("annaId", "lucyId")
-        )
-        return followings
-            .filter { it.userId == userId }
-            .map { it.followedId }
     }
 }
