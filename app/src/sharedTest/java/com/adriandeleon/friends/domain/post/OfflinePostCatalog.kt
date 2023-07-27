b538fc1675f7c4972eac1f9d0d0f2e86fa85bcf1
Adrian De León <adriandleon@gmail.com>
2023-07-26 22:20:02 -0300

    Extracted unavailable and offline post catalog as shared test-doubles

---
+++ app/src/sharedTest/java/com/adriandeleon/friends/domain/post/OfflinePostCatalog.kt
@@ -0,0 +1,13 @@
+package com.adriandeleon.friends.domain.post
+
+import com.adriandeleon.friends.domain.exceptions.ConnectionUnavailableException
+
+class OfflinePostCatalog : PostCatalog {
+    override fun addPost(userId: String, postText: String): Post {
+        throw ConnectionUnavailableException()
+    }
+
+    override suspend fun postsFor(userIds: List<String>): List<Post> {
+        throw ConnectionUnavailableException()
+    }
+}
