e1b171fedcf650e757a3ef26dd6aa73bb0fde342
Adrian De León <adriandleon@gmail.com>
2023-11-20 11:07:44 -0300

    Extracted the data outside of the test

--- app/src/test/java/com/adriandeleon/friends/domain/post/InMemoryPostCatalogTest.kt
+++ app/src/test/java/com/adriandeleon/friends/domain/post/InMemoryPostCatalogTest.kt
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test
 
 class InMemoryPostCatalogTest {
 
+    private val tomId = "tomId"
+    private val tomPost = Post("tomPostId", tomId, "Post by Tom", 1L)
+    private val annaPost = Post("annaPostId", "annaId", "Post by Anna", 2L)
+    private val lucyPost = Post("lucyPostId", "lucyId", "Post by Lucy", 3L)
+
     @Test
     fun `post found`() = runBlocking {
-        val tomId = "tomId"
-        val tomPost = Post("tomPostId", tomId, "Post by Tom", 1L)
-        val annaPost = Post("annaPostId", "annaId", "Post by Anna", 2L)
-        val lucyPost = Post("lucyPostId", "lucyId", "Post by Lucy", 3L)
         val postCatalog = InMemoryPostCatalog(
             availablePosts = listOf(annaPost, tomPost, lucyPost)
         )
