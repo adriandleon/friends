23bff8ec49589316bcb5ccd861b6858ad7ce202f
Adrian De León <adriandleon@gmail.com>
2024-03-13 16:25:07 -0300

    Improved names, made duplication more obvious

--- app/src/test/java/com/adriandeleon/friends/people/LoadPeopleTest.kt
+++ app/src/test/java/com/adriandeleon/friends/people/LoadPeopleTest.kt
@@ -22,25 +22,25 @@ class LoadPeopleTest {
 
     @Test
     fun `loaded a single person`() {
-        val tomFriend = Friend(User("tomId", "", ""), isFollowee = false)
+        val tom = Friend(User("tomId", "", ""), isFollowee = false)
         val viewModel = PeopleViewModel()
 
         viewModel.loadPeople("annaId")
 
-        assertEquals(PeopleState.Loaded(listOf(tomFriend)), viewModel.peopleState.value)
+        assertEquals(PeopleState.Loaded(listOf(tom)), viewModel.peopleState.value)
     }
 
     @Test
     fun `loaded multiple people`() {
-        val friendAnna = Friend(User("annaId", "", ""), isFollowee = true)
-        val friendSara = Friend(User("saraId", "", ""), isFollowee = false)
-        val friendTom = Friend(User("tomId", "", ""), isFollowee = false)
+        val anna = Friend(User("annaId", "", ""), isFollowee = true)
+        val sara = Friend(User("saraId", "", ""), isFollowee = false)
+        val tom = Friend(User("tomId", "", ""), isFollowee = false)
         val viewModel = PeopleViewModel()
 
         viewModel.loadPeople("lucyId")
 
         assertEquals(
-            PeopleState.Loaded(listOf(friendAnna, friendSara, friendTom)),
+            PeopleState.Loaded(listOf(anna, sara, tom)),
             viewModel.peopleState.value
         )
     }
