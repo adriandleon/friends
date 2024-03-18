6a0703fe0ed5a959867d1298f82c904c62d6aa6c
Adrian De León <adriandleon@gmail.com>
2024-03-18 12:27:48 -0300

    Introduced a class to hold the responsibility of translating domain data into ui data

--- app/src/main/java/com/adriandeleon/friends/people/PeopleViewModel.kt
+++ app/src/main/java/com/adriandeleon/friends/people/PeopleViewModel.kt
@@ -4,8 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.adriandeleon.friends.domain.exceptions.BackendException
 import com.adriandeleon.friends.domain.exceptions.ConnectionUnavailableException
-import com.adriandeleon.friends.domain.user.Friend
-import com.adriandeleon.friends.domain.user.User
+import com.adriandeleon.friends.domain.people.InMemoryPeopleCatalog
 import com.adriandeleon.friends.people.state.PeopleState
 
 class PeopleViewModel {
@@ -13,31 +12,20 @@ class PeopleViewModel {
     val peopleState: LiveData<PeopleState> = mutablePeopleState
 
     fun loadPeople(userId: String) {
-        val result = try {
-            val peopleForUserId = InMemoryPeopleCatalog().loadPeopleFor(userId)
-            PeopleState.Loaded(peopleForUserId)
-        } catch (backendException: BackendException) {
-            PeopleState.BackendError
-        } catch (offlineException: ConnectionUnavailableException) {
-            PeopleState.Offline
-        }
-
+        val result = PeopleRepository().loadPeopleFor(userId)
         mutablePeopleState.value = result
     }
 
-    class InMemoryPeopleCatalog {
-        private val tom = Friend(User("tomId", "", ""), isFollowee = false)
-        private val anna = Friend(User("annaId", "", ""), isFollowee = true)
-        private val sara = Friend(User("saraId", "", ""), isFollowee = false)
-        private val peopleForUserId = mapOf(
-            "annaId" to listOf(tom),
-            "lucyId" to listOf(anna, sara, tom),
-            "saraId" to emptyList()
-        )
-
-        fun loadPeopleFor(userId: String): List<Friend> {
-            if (userId.isBlank()) throw ConnectionUnavailableException()
-            return peopleForUserId[userId] ?: throw BackendException()
+    class PeopleRepository {
+        fun loadPeopleFor(userId: String): PeopleState {
+            return try {
+                val peopleForUserId = InMemoryPeopleCatalog().loadPeopleFor(userId)
+                PeopleState.Loaded(peopleForUserId)
+            } catch (backendException: BackendException) {
+                PeopleState.BackendError
+            } catch (offlineException: ConnectionUnavailableException) {
+                PeopleState.Offline
+            }
         }
     }
 }
