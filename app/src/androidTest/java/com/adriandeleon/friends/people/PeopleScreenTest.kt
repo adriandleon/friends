4d23f49d55ccbb3fc9521ddf1bf247a56f44f5f9
Adrian De León <adriandleon@gmail.com>
2024-03-25 11:01:32 -0300

    Improved the intention of the test and removed obsolete code

--- app/src/androidTest/java/com/adriandeleon/friends/people/PeopleScreenTest.kt
+++ app/src/androidTest/java/com/adriandeleon/friends/people/PeopleScreenTest.kt
@@ -2,6 +2,7 @@ package com.adriandeleon.friends.people
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.adriandeleon.friends.MainActivity
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -10,6 +11,7 @@ class PeopleScreenTest {
     @get:Rule
     val rule = createAndroidComposeRule<MainActivity>()
 
+    @Ignore("Test incomplete")
     @Test
     fun displaysPeople() {
         launchPeopleFor("email@email.com", rule) {
--- app/src/test/java/com/adriandeleon/friends/people/FailPeopleLoadingTest.kt
+++ app/src/test/java/com/adriandeleon/friends/people/FailPeopleLoadingTest.kt
@@ -1,12 +1,11 @@
 package com.adriandeleon.friends.people
 
 import com.adriandeleon.friends.InstantTaskExecutor
+import com.adriandeleon.friends.domain.exceptions.BackendException
 import com.adriandeleon.friends.domain.exceptions.ConnectionUnavailableException
-import com.adriandeleon.friends.domain.people.InMemoryPeopleCatalog
 import com.adriandeleon.friends.domain.people.PeopleCatalog
 import com.adriandeleon.friends.domain.people.PeopleRepository
 import com.adriandeleon.friends.domain.user.Friend
-import com.adriandeleon.friends.domain.user.User
 import com.adriandeleon.friends.people.state.PeopleState
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -15,23 +14,11 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(InstantTaskExecutor::class)
 class FailPeopleLoadingTest {
 
-    private val tom = Friend(User("tomId", "", ""), isFollowee = false)
-    private val anna = Friend(User("annaId", "", ""), isFollowee = true)
-    private val sara = Friend(User("saraId", "", ""), isFollowee = false)
-
-    private val peopleCatalog = InMemoryPeopleCatalog(
-        mapOf(
-            "annaId" to listOf(tom),
-            "lucyId" to listOf(anna, sara, tom),
-            "saraId" to emptyList()
-        )
-    )
-
     @Test
     fun `backend error`() {
-        val viewModel = PeopleViewModel(PeopleRepository(peopleCatalog))
+        val viewModel = PeopleViewModel(PeopleRepository(UnavailablePeopleCatalog()))
 
-        viewModel.loadPeople("johnId")
+        viewModel.loadPeople(":irrelevant:")
 
         assertEquals(PeopleState.BackendError, viewModel.peopleState.value)
     }
@@ -43,6 +30,12 @@ class FailPeopleLoadingTest {
         viewModel.loadPeople(":irrelevant:")
 
         assertEquals(PeopleState.Offline, viewModel.peopleState.value)
+    }
+
+    private class UnavailablePeopleCatalog : PeopleCatalog {
+        override fun loadPeopleFor(userId: String): List<Friend> {
+            throw BackendException()
+        }
     }
 
     private class OfflinePeopleCatalog : PeopleCatalog {
